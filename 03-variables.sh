e9abf4b677124af61b61642b3ba7338cda70ac07
anithas1214 <anithas0923@gmail.com>
2025-01-17 00:12:08 +0530

    3rd script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1: how are you suresh"
+echo "$person2: i am good,how about you ramesh"
+echo "$person1: I am good how is your work"
+echo "$person2: I am ok,looking for upgrade devops"
