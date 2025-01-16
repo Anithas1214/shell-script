3bb0c5fa22d790c1f5408a19e2cad16bde432f27
anithas1214 <anithas0923@gmail.com>
2025-01-17 00:08:18 +0530

    2nd script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh: how are you suresh"
+echo "suresh: i am good,how about you ramesh"
+echo "ramesh: I am good how is your work"
+echo "suresh: I am ok,looking for upgrade devops"
