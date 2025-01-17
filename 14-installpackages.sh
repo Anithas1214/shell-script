9c665b399fdf35263883ff517edc1928fe6d226c
anithas1214 <anithas0923@gmail.com>
2025-01-17 23:41:32 +0530

    packages

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+script_name=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$script_name-$timestamp.log
+
+validate(){
+   if [ $1 -ne 0 ]
+   then
+      echo "$2......failure"
+      exit 1
+   else
+     echo "$2......success"
+   fi     
+}
 
 if [ $userid -ne 0 ]
 then
@@ -15,4 +28,10 @@ echo "all packages: $@"
 for i in $@
 do
  echo "package to install: $i"
+ dnf list installed $i &>>$logfile
+ if [ $? -eq 0 ]
+ then
+    echo "$i already installed.....skipping"
+ else
+      echo "$i not installed.....need to install" 
 done 
