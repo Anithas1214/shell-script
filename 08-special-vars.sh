05a1d63177db8a1a593e70163e948cb786440c45
anithas1214 <anithas0923@gmail.com>
2025-01-17 11:49:29 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -8,3 +8,22 @@ echo "No of variables: $#"
 
 #$0 is symbol of script name
 echo "script name: $0"
+
+#$PWD is current working directory
+echo "current working directory: $PWD"
+
+#$HOME is home directory of current user
+echo "home directory of current user: $HOME"
+
+#$USER which user is running this script 
+echo "which user is running this script : $USER"
+
+#$HOSTNAME server name
+echo "server name : $HOSTNAME"
+
+#$$ process id of shellscript
+echo "process id of shellscript : $$"
+sleep 60 &
+
+#$! process id of last background command
+echo "process id of last background command : $!"
