2e34e92ad3cc6fd734ef3ffdfa46bf7d0decb80f
anithas1214 <anithas0923@gmail.com>
2025-01-17 16:11:09 +0530

    exit status check

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,5 +7,11 @@ then
 else
    echo " you are super user "   
 fi   
-dnf install mysql -y
+dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+   echo "installtion of mysql ..........failure"
+   exit 1
+fi
+dnf install git -y   
 echo "is script is proceeding?"
